a9d21454a0efff187ebbb74d9db5ba4225fc229a
Eugene L <lzrefs@gmail.com>
2020-12-03 23:14:32 -0800

    Refactored MidiEncoder into a separate file

---
+++ firmware/include/MidiEncoder.h
@@ -0,0 +1,86 @@
+#ifndef _MIDI_ENCODER_H_
+#define _MIDI_ENCODER_H_
+
+#include <Arduino.h>
+#include "Debug.h"
+
+//#define ENC_DECODER ENC_FLAKY
+//#define ENC_HALFSTEP
+#include <ClickEncoder.h>
+
+typedef void (*onEncoderTurnedHandlerT)(unsigned int midiCc, int delta);
+typedef void (*onEncoderPressedHandlerT)(unsigned int midiCc);
+typedef void (*onEncoderReleasedHandlerT)(unsigned int midiCc);
+
+class MidiEncoder
+{
+    onEncoderTurnedHandlerT onEncoderTurnedHandler = 0;
+    onEncoderPressedHandlerT onEncoderPressedHandler = 0;
+    onEncoderReleasedHandlerT onEncoderReleasedHandler = 0;
+
+public:
+    MidiEncoder(int cc, int pinA, int pinB, int pinButton) : physicalEncoder(pinA, pinB, -1), midiCc(cc), buttonPin(pinButton)
+    {
+        pinMode(pinButton, INPUT_PULLUP);
+    }
+
+    void setOnEncoderTurnedHandler(onEncoderTurnedHandlerT handler)
+    {
+        onEncoderTurnedHandler = handler;
+    }
+
+    void setOnEncoderPressedHandler(onEncoderPressedHandlerT handler)
+    {
+        onEncoderPressedHandler = handler;
+    }
+
+    void setOnEncoderReleasedHandler(onEncoderReleasedHandlerT handler)
+    {
+        onEncoderReleasedHandler = handler;
+    }
+
+    void service()
+    {
+        physicalEncoder.service();
+
+        int encoderDelta = physicalEncoder.getValue() * 5;
+        if (encoderDelta != 0)
+        {
+            if (onEncoderTurnedHandler) {
+                onEncoderTurnedHandler(midiCc, encoderDelta);
+            }
+        }
+
+        int buttonState = digitalRead(buttonPin);
+        int curTime = millis();
+        const int debouncePeriod = 20;
+
+        if (buttonState != lastButtonState && curTime - lastButtonEventTime > debouncePeriod)
+        {
+            lastButtonState = buttonState;
+            lastButtonEventTime = curTime;
+
+            if (LOW == buttonState)
+            {
+                if (onEncoderPressedHandler) {
+                    onEncoderPressedHandler(midiCc);
+                }
+            }
+            else if (HIGH == buttonState)
+            {
+                if (onEncoderReleasedHandler) {
+                    onEncoderReleasedHandler(midiCc);
+                }
+            }
+        }
+    }
+
+protected:
+    ClickEncoder physicalEncoder;
+    int midiCc;
+    int buttonPin;
+    int lastButtonState = HIGH;
+    int lastButtonEventTime = 0;
+};
+
+#endif
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,78 +2,23 @@
 #include "Display.h"
 #include "Midi.h"
 #include "SysexParser.h"
-
-//#define ENC_DECODER ENC_FLAKY
-//#define ENC_HALFSTEP
-#include <ClickEncoder.h>
+#include "MidiEncoder.h"
 
 Display display;
 myMidi midi;
 SysexParser sysexParser;
 
-class MidiEncoder
-{
-public:
-  MidiEncoder(int cc, int pinA, int pinB, int pinButton):
-    physicalEncoder(pinA, pinB, -1), midiCc(cc), buttonPin(pinButton)
-  {
-    pinMode(pinButton, INPUT_PULLUP);
-  }
-
-  void service()
-  {
-    physicalEncoder.service();
-
-    int encoderDelta = physicalEncoder.getValue() * 5;
-    int encoderRelative = encoderDelta >= 0 ? encoderDelta : 128 + encoderDelta;
-
-    if (encoderDelta != 0) {
-      midi.sendControlChange(15, midiCc, encoderRelative);
-      DBG("Sending encoder ");
-      DBG(midiCc);
-      DBG(" val: ");
-      DBGL(encoderRelative);
-    }
-
-    int buttonState = digitalRead(buttonPin);
-    int curTime = millis();
-    const int debouncePeriod = 20;
-
-    if (buttonState != lastButtonState && curTime - lastButtonEventTime > debouncePeriod) {
-      lastButtonState = buttonState;
-      lastButtonEventTime = curTime;
-
-      if (LOW == buttonState) {
-        DBG("Button pressed: ");
-        DBGL(midiCc);
-        midi.sendNoteOn(15, midiCc, 127);
-      } else if (HIGH == buttonState) {
-        DBG("Button released: ");
-        DBGL(midiCc);
-        midi.sendNoteOff(15, midiCc, 0);
-      }
-    }
-  }
-
-protected:
-  ClickEncoder physicalEncoder;
-  int midiCc;
-  int buttonPin;
-  int lastButtonState = HIGH;
-  int lastButtonEventTime = 0;
-};
-
 const int numEncoders = 8;
 
 MidiEncoder encoders[numEncoders] = {
-  MidiEncoder(21, PB1,	PB10,	PB11),
-  MidiEncoder(22, PA15,	PB4,	PB12),
-  MidiEncoder(23, PA10,	PB13,	PA9),
-  MidiEncoder(24, PC14,	PC15,	PC13),
-  MidiEncoder(25, PB6,	PB8,	PB0),
-  MidiEncoder(26, PB9,	PB7,	PB5),
-  MidiEncoder(27, PA8,	PB15,	PB14),
-  MidiEncoder(28, PA1,	PA2,	PA0),
+    MidiEncoder(21, PB1, PB10, PB11),
+    MidiEncoder(22, PA15, PB4, PB12),
+    MidiEncoder(23, PA10, PB13, PA9),
+    MidiEncoder(24, PC14, PC15, PC13),
+    MidiEncoder(25, PB6, PB8, PB0),
+    MidiEncoder(26, PB9, PB7, PB5),
+    MidiEncoder(27, PA8, PB15, PB14),
+    MidiEncoder(28, PA1, PA2, PA0),
 };
 
 void onTextRefreshHandler(int hPosition, int vPosition, String text)
@@ -95,7 +40,8 @@ const int encoder0Cc = 21;
 
 void onCc(unsigned int channel, unsigned int controller, unsigned int value)
 {
-  if (controller >= encoder0Cc && controller < encoder0Cc + 8) {
+  if (controller >= encoder0Cc && controller < encoder0Cc + 8)
+  {
     int encoderIndex = controller - encoder0Cc;
 
     DBG("Received encoder value: ");
@@ -105,6 +51,32 @@ void onCc(unsigned int channel, unsigned int controller, unsigned int value)
 
     display.showValue(encoderIndex, value);
   }
+}
+
+void onEncoderTurnedHandler(unsigned int midiCc, int delta)
+{
+  int relativeValue = delta >= 0 ? delta : 128 + delta;
+
+  midi.sendControlChange(15, midiCc, relativeValue);
+
+  DBG("Sending encoder ");
+  DBG(midiCc);
+  DBG(" val: ");
+  DBGL(relativeValue);
+}
+
+void onEncoderPressedHandler(unsigned int midiCc)
+{
+  DBG("Button pressed: ");
+  DBGL(midiCc);
+  midi.sendNoteOn(15, midiCc, 127);
+}
+
+void onEncoderReleasedHandler(unsigned int midiCc)
+{
+  DBG("Button released: ");
+  DBGL(midiCc);
+  midi.sendNoteOff(15, midiCc, 0);
 }
 
 USBCompositeSerial CompositeSerial;
@@ -129,12 +101,20 @@ void setup()
   midi.setOnCcHandler(onCc);
   sysexParser.setOnTextRefreshHandler(onTextRefreshHandler);
 
+  for (int i = 0; i < numEncoders; i++)
+  {
+    encoders[i].setOnEncoderTurnedHandler(onEncoderTurnedHandler);
+    encoders[i].setOnEncoderPressedHandler(onEncoderPressedHandler);
+    encoders[i].setOnEncoderReleasedHandler(onEncoderReleasedHandler);
+  }
+
   DBGL("...hooks are set up. Setup is finished");
 }
 
 void serviceEncoders()
 {
-  for (int i = 0; i < numEncoders; i++) {
+  for (int i = 0; i < numEncoders; i++)
+  {
     encoders[i].service();
   }
 }
