c31d251abb49101ee0a758e8332e315bd005a911
lzref <evgenius.lazarchik@gmail.com>
2021-11-02 20:03:28 -0700

    Adding back Display.h and Midi.h

---
+++ firmware/include/Display.h
@@ -0,0 +1,118 @@
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Debug.h"
+
+#include <Adafruit_ILI9341.h>
+
+#define TFT_DC PA3
+
+#define TFT_CS 0
+
+#define TFT_MOSI PA7
+#define TFT_CLK PA5
+
+#define TFT_RST 0
+#define TFT_MISO PA6
+
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+const int xDist = 80;
+const int yDist = 40;
+const int yValDist = 20;
+const int textSize = 1;
+const int outerBoxSizeX = 76;
+const int outerBoxSizeY = 19;
+const int innerBoxSizeX = outerBoxSizeX - 4;
+const int innerBoxSizeY = outerBoxSizeY - 4;
+
+
+class Display: Adafruit_ILI9341 {
+public:
+    Display(): Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST) {
+    }
+
+    int boxCoordX(int x, int y)
+    {
+        return 2 + xDist * (x % 4);
+    }
+
+    int boxCoordY(int x, int y)
+    {
+        return 2 + yDist * (2 * (x / 4)) + yValDist * y;
+    }
+
+    String repeatString(String source, unsigned int count)
+    {
+        String result;
+        for (unsigned int i = 0; i <= count; i++) {
+            result += source;
+        }
+
+        return result;
+    }
+
+    void printMessage(int x, int y, String msg)
+    {
+        int xout = boxCoordX(x, y);
+        int yout = boxCoordY(x, y);
+
+        if (msg.length() < 9) {
+            msg += repeatString(String(" "), 9 - msg.length());
+        }
+
+        setTextSize(textSize);
+        setTextColor(YELLOW, BLACK);
+        setCursor(xout + 3, yout + 3);
+        println(msg.c_str());
+    }
+
+    void drawBoundingBox(int x, int y)
+    {
+        int xout = boxCoordX(x, y);
+        int yout = boxCoordY(x, y);
+
+        fillRect(xout, yout, outerBoxSizeX, outerBoxSizeY, BLACK);
+    }
+
+    void showValue(int x, int oldValue, int newValue)
+    {
+        int xout = boxCoordX(x, 2);
+        int yout = boxCoordY(x, 2);
+
+        int oldValueX = xout + 2 + (innerBoxSizeX * oldValue / 127);
+        int newValueX = xout + 2 + (innerBoxSizeX * newValue / 127);
+
+        if (newValue > oldValue) {
+            fillRect(oldValueX, yout + 2, newValueX - oldValueX, innerBoxSizeY, YELLOW);
+        } else {
+            fillRect(newValueX, yout + 2, oldValueX - newValueX, innerBoxSizeY, BLACK);
+        }
+    }
+
+    void setup() {
+        begin();
+
+        fillScreen(GREEN);
+        setRotation(3);
+        //invertDisplay(true);
+
+        for (int i = 0; i <= 8; i++) {
+            drawBoundingBox(i, 0);
+            drawBoundingBox(i, 1);
+            drawBoundingBox(i, 2);
+        }
+    }
+};
+
+#endif
---
+++ firmware/include/Midi.h
@@ -0,0 +1,203 @@
+#ifndef _MIDI_H_
+#define _MIDI_H_
+
+#include <Arduino.h>
+#include "USBComposite.h"
+#include "usb_midi_device.h"
+#include "SysexParser.h"
+
+union EVENT_t
+{
+    uint32 i;
+    uint8 b[4];
+    MIDI_EVENT_PACKET_t p;
+};
+
+typedef void (*onSysexDataHandlerT)(unsigned char data);
+typedef void (*onSysexEndHandlerT)(void);
+typedef void (*onCcHandlerT)(unsigned int channel, unsigned int controller, unsigned int value);
+
+class myMidi : public USBMIDI
+{
+    onSysexDataHandlerT onSysexDataHandler = 0;
+    onSysexEndHandlerT onSysexEndHandler = 0;
+    onCcHandlerT onCcHandler = 0;
+
+    virtual void handleNoteOff(unsigned int channel, unsigned int note, unsigned int velocity)
+    {
+        DBGL("noteOff");
+    }
+
+    virtual void handleNoteOn(unsigned int channel, unsigned int note, unsigned int velocity)
+    {
+        DBGL("NoteOn");
+    }
+
+    void handleControlChange(unsigned int channel, unsigned int controller, unsigned int value) {
+        if (onCcHandler) {
+            onCcHandler(channel, controller, value);
+        }
+    }
+
+    virtual void handleSysExEnd(void)
+    {
+        if (onSysexEndHandler)
+        {
+            onSysexEndHandler();
+        }
+    }
+
+    virtual void handleSysExData(unsigned char data)
+    {
+        if (onSysexDataHandler)
+        {
+            onSysexDataHandler(data);
+        }
+    }
+
+public:
+    void setOnSysexEndHandler(onSysexEndHandlerT handler)
+    {
+        onSysexEndHandler = handler;
+    }
+
+    void setOnSysexDataHandler(onSysexDataHandlerT handler)
+    {
+        onSysexDataHandler = handler;
+    }
+    
+    void setOnCcHandler(onCcHandlerT handler)
+    {
+        onCcHandler = handler;
+    }
+
+    void setup()
+    {
+        registerComponent();
+    }
+
+    void dispatchPacket(uint32 p)
+    {
+        union EVENT_t e;
+
+        e.i = p;
+        switch (e.p.cin)
+        {
+        case CIN_SYSEX:
+            //DBGL("SysEx");
+            handleSysExData(e.p.midi0);
+            handleSysExData(e.p.midi1);
+            handleSysExData(e.p.midi2);
+            break;
+        case CIN_SYSEX_ENDS_IN_1:
+            //DBGL("SysEx1");
+            handleSysExData(e.p.midi0);
+            handleSysExEnd();
+            break;
+        case CIN_SYSEX_ENDS_IN_2:
+            //DBGL("SysEx2");
+            handleSysExData(e.p.midi0);
+            handleSysExData(e.p.midi1);
+            handleSysExEnd();
+            break;
+        case CIN_SYSEX_ENDS_IN_3:
+            //DBGL("SysEx3");
+            handleSysExData(e.p.midi0);
+            handleSysExData(e.p.midi1);
+            handleSysExData(e.p.midi2);
+            handleSysExEnd();
+            break;
+        case CIN_3BYTE_SYS_COMMON:
+            DBGL("CIN_3BYTE_SYS_COMMON");
+            if (e.p.midi0 == MIDIv1_SONG_POSITION_PTR)
+            {
+                handleSongPosition(((uint16)e.p.midi2) << 7 | ((uint16)e.p.midi1));
+            }
+            break;
+
+        case CIN_2BYTE_SYS_COMMON:
+            DBGL("CIN_2BYTE_SYS_COMMON");
+            switch (e.p.midi0)
+            {
+            case MIDIv1_SONG_SELECT:
+                handleSongSelect(e.p.midi1);
+                break;
+            case MIDIv1_MTC_QUARTER_FRAME:
+                // reference library doesnt handle quarter frame.
+                break;
+            }
+            break;
+        case CIN_NOTE_OFF:
+            handleNoteOff(MIDIv1_VOICE_CHANNEL(e.p.midi0), e.p.midi1, e.p.midi2);
+            break;
+        case CIN_NOTE_ON:
+            handleNoteOn(MIDIv1_VOICE_CHANNEL(e.p.midi0), e.p.midi1, e.p.midi2);
+            break;
+        case CIN_AFTER_TOUCH:
+            handleVelocityChange(MIDIv1_VOICE_CHANNEL(e.p.midi0), e.p.midi1, e.p.midi2);
+            break;
+        case CIN_CONTROL_CHANGE:
+            handleControlChange(MIDIv1_VOICE_CHANNEL(e.p.midi0), e.p.midi1, e.p.midi2);
+            break;
+        case CIN_PROGRAM_CHANGE:
+            handleProgramChange(MIDIv1_VOICE_CHANNEL(e.p.midi0), e.p.midi1);
+            break;
+        case CIN_CHANNEL_PRESSURE:
+            handleAfterTouch(MIDIv1_VOICE_CHANNEL(e.p.midi0), e.p.midi1);
+            break;
+
+        case CIN_PITCH_WHEEL:
+            handlePitchChange(((uint16)e.p.midi2) << 7 | ((uint16)e.p.midi1));
+            break;
+        case CIN_1BYTE:
+            switch (e.p.midi0)
+            {
+            case MIDIv1_CLOCK:
+                handleSync();
+                break;
+            case MIDIv1_TICK:
+                break;
+            case MIDIv1_START:
+                handleStart();
+                break;
+            case MIDIv1_CONTINUE:
+                handleContinue();
+                break;
+            case MIDIv1_STOP:
+                handleStop();
+                break;
+            case MIDIv1_ACTIVE_SENSE:
+                handleActiveSense();
+                break;
+            case MIDIv1_RESET:
+                handleReset();
+                break;
+            case MIDIv1_TUNE_REQUEST:
+                handleTuneRequest();
+                break;
+
+            default:
+                DBG("Invalid CIN_1BYTE packet: ");
+                DBGL(e.p.midi0);
+                break;
+            }
+            break;
+
+        default:
+            DBG("Invalid SysEx packet ");
+            DBGL(e.p.cin);
+            break;
+        }
+    }
+
+    // Try to read data from USB port & pass anything read to processing function
+    void poll(void)
+    {
+        while (available())
+        {
+            dispatchPacket(readPacket());
+        }
+    }
+};
+
+#endif
